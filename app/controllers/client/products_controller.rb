7b56c8d4b4e0bce933b197fc423887434e2b4dbc
Peter Jang <peter.x.jang@gmail.com>
2018-04-24 15:33:05 -0500

    Add client products new and create actions

--- app/controllers/client/products_controller.rb
+++ app/controllers/client/products_controller.rb
@@ -5,6 +5,21 @@ class Client::ProductsController < ApplicationController
     render 'index.html.erb'
   end
 
+  def new
+    render "new.html.erb"    
+  end
+
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description]
+    )
+    @product.save
+    render "create.html.erb"
+  end
+
   def show
     response = Unirest.get("http://localhost:3000/api/products/#{params[:id]}")
     @product = response.body
