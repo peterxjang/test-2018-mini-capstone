0abb380ef44921265c81c2261a4afe488f99433f
Peter Jang <peter.x.jang@gmail.com>
2018-04-24 15:09:57 -0500

    Add client products index action

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
     patch "/products/:id" => "products#update"
     delete "/products/:id" => "products#destroy"
   end
+
+  namespace :client do
+    get '/products' => 'products#index'
+  end
 end
