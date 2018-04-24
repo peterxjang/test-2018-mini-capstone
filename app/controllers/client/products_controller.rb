5c2c4e2268b1cdebf46550c851a2ccca3b34dd98
Peter Jang <peter.x.jang@gmail.com>
2018-04-24 15:19:00 -0500

    Add client products show action

--- app/controllers/client/products_controller.rb
+++ app/controllers/client/products_controller.rb
@@ -4,4 +4,10 @@ class Client::ProductsController < ApplicationController
     @products = response.body
     render 'index.html.erb'
   end
+
+  def show
+    response = Unirest.get("http://localhost:3000/api/products/#{params[:id]}")
+    @product = response.body
+    render "show.html.erb"
+  end
 end
