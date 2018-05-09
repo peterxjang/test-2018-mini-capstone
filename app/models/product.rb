1a308dfe47836e81d7c661db990e65a73e187530
Peter Jang <peter.x.jang@gmail.com>
2018-05-08 19:22:46 -0500

    Add supplier model with association to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,8 @@ class Product < ApplicationRecord
   validates :description, presence: true
   validates :description, length: { in: 10..500 }
 
+  belongs_to :supplier
+
   def is_discounted
     price.to_f <= 5
   end
