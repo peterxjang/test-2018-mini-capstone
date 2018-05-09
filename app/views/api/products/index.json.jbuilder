4fc4150ee18ceb0355e8577d47044a5e9ee5a50f
Peter Jang <peter.x.jang@gmail.com>
2018-05-08 21:04:24 -0500

    Add image associated with products and display on products index and show pages

--- app/views/api/products/index.json.jbuilder
+++ app/views/api/products/index.json.jbuilder
@@ -2,8 +2,8 @@ json.array! @products.each do |product|
   json.id product.id
   json.name product.name
   json.price product.price
-  json.image_url product.image_url
   json.description product.description
+  json.images product.images
 
   json.is_discounted product.is_discounted
   json.tax product.tax
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180509001941) do
+ActiveRecord::Schema.define(version: 20180509003501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "images", force: :cascade do |t|
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "product_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.decimal "price", precision: 9, scale: 2
-    t.string "image_url"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
